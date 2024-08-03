0235e0f856361c84504045d49c7b398b6fdb4a60
Lazarte, Hector <hlazarte@everfi.com>
2024-08-03 18:54:01 -0400

    El menú se autocolapse

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 function NavBar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+  const handleSelect = () => setExpanded(false);
+
   return (
-    <Navbar className="navbar" expand="lg">
+    <Navbar className="navbar" expand="lg" expanded={expanded}>
       <Navbar.Brand as={Link} to="/" className="custom-brand">Tu Tienda en Línea</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" onClick={handleToggle} />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-          <Nav.Link as={Link} to="/products">Productos</Nav.Link>
-          <Nav.Link as={Link} to="/cart">Carrito</Nav.Link>
-          <Nav.Link as={Link} to="/profile">Perfil</Nav.Link>
-          <Nav.Link as={Link} to="/login">Iniciar Sesión</Nav.Link>
+        <Nav className="mr-auto" onClick={handleSelect}>
+          <Nav.Link as={Link} to="/" className="custom-nav-link">Inicio</Nav.Link>
+          <Nav.Link as={Link} to="/products" className="custom-nav-link">Productos</Nav.Link>
+          <Nav.Link as={Link} to="/cart" className="custom-nav-link">Carrito</Nav.Link>
+          <Nav.Link as={Link} to="/profile" className="custom-nav-link">Perfil</Nav.Link>
+          <Nav.Link as={Link} to="/login" className="custom-nav-link">Iniciar Sesión</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
