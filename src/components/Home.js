146bbcbc35cae67c3ca4eec2ab437e4a6de1bacb
Lazarte, Hector <hlazarte@everfi.com>
2024-09-11 20:00:49 -0400

    Orders y Otros

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,8 @@ function Home() {
             alt="Imagen 1"
           />
           <div className="carousel-text">
-            <h3>Primera Imagen</h3>
-            <p>Paquete de 2 organizadores y almacenamiento para debajo del fregadero, organizador deslizante de 2 niveles con...</p>
+            <h3>Purificador de Aire</h3>
+            <p>Potente filtración: el filtro PuroAir HEPA captura todo lo que no quieres respirar. Filtra el polvo, el polen, el humo, los compuestos orgánicos volátiles y todo lo que no quieres respirar. Se muestra que captura hasta el 99% de partículas que se encuentran...</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
@@ -25,8 +25,8 @@ function Home() {
             alt="Imagen 2"
           />
           <div className="carousel-text">
-            <h3>Segunda Imagen</h3>
-            <p>Paquete de 2 organizadores y almacenamiento para debajo del fregadero, organizador deslizante de 2 niveles con...</p>
+            <h3>Licuadora de Alta Potencia</h3>
+            <p>Mezclador de encimera de cocina: la licuadora de grado profesional utiliza botones de 1 toque para controlar los 6 ciclos preprogramados, la opción de pulso y el control manual de 10 velocidades...</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
@@ -36,8 +36,8 @@ function Home() {
             alt="Imagen 3"
           />
           <div className="carousel-text">
-            <h3>Tercera Imagen</h3>
-            <p>Paquete de 2 organizadores y almacenamiento para debajo del fregadero, organizador deslizante de 2 niveles con...</p>
+            <h3>Set de Cuchillos de Cocina</h3>
+            <p>Juego de cuchillos de cocina de Damasco de 18 piezas, 8 cuchillos de carne, mango ergonómico de ABS antideslizante triple remache para tenedor de carne, afilador de cuchillos y tijeras, bloque de...</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
@@ -47,8 +47,8 @@ function Home() {
             alt="Imagen 4"
           />
           <div className="carousel-text">
-            <h3>Cuarta Imagen</h3>
-            <p>Paquete de 2 organizadores y almacenamiento para debajo del fregadero, organizador deslizante de 2 niveles con...</p>
+            <h3>Robot Aspirador</h3>
+            <p>Aspirador robot con succión de 3000 Pa, ajustes de limpieza personalizados, aspiradora robótica de carga automática, refuerzo de alfombras, aplicación/Alexa/control remoto...</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
@@ -58,8 +58,8 @@ function Home() {
             alt="Imagen 5"
           />
           <div className="carousel-text">
-            <h3>Quinta Imagen</h3>
-            <p>Paquete de 2 organizadores y almacenamiento para debajo del fregadero, organizador deslizante de 2 niveles con...</p>
+            <h3>Funda de Almohada de Seda</h3>
+            <p>Zimasilk - Funda de almohada de seda 100% morera para la salud del cabello y la piel, seda de ambos lados de 19 mommes, 1 pieza (King 20” x 36”, verde menta)...</p>
           </div>
         </Carousel.Item>
       </Carousel>
