9fe81a40ad70ca18e133fc9b0381c25034c34c19
Lazarte, Hector <hlazarte@everfi.com>
2025-03-25 13:12:39 -0400

    Citas

--- src/mod/CitasComponent.js
+++ src/mod/CitasComponent.js
@@ -8,7 +8,10 @@ const CitasComponent = () => {
   const [showDialog, setShowDialog] = useState(false)
   const [selectedCita, setSelectedCita] = useState(null)
   const [showSpinner, setShowSpinner] = useState(false)
-  const pacienteId = 48
+  const [pacientes, setPacientes] = useState([])
+  const [pacienteSeleccionado, setPacienteSeleccionado] = useState(null)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [infoMsg, setInfoMsg] = useState('')
 
   const ESPECIALIDADES = [
     { id: 1, nombre: 'Reumatólogo' },
@@ -42,7 +45,43 @@ const CitasComponent = () => {
 
     fetchCitas()
   }, [especialidadId])
-
+  
+  useEffect(() => {
+    const fetchPacientes = async () => {
+      const token = localStorage.getItem('access_token')
+      setShowSpinner(true)
+      const response = await fetch(`${window.infoConfig.apiUrl}/pacientes/filtrar?page=1&per_page=100`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ paciente: {} })
+      })
+      setShowSpinner(false)
+  
+      if (response.status === 401) {
+        window.location.href = '/'
+        return
+      }
+  
+      if (response.ok) {
+        const data = await response.json()
+        // Ordenamos por nombre y apellido
+        const ordenados = data.results.sort((a, b) => {
+          const nombreA = `${a.nombre} ${a.apellido_paterno}`.toLowerCase()
+          const nombreB = `${b.nombre} ${b.apellido_paterno}`.toLowerCase()
+          return nombreA.localeCompare(nombreB)
+        })
+  
+        setPacientes(ordenados)
+        setPacienteSeleccionado(ordenados[0]) // Selecciona el primero
+      }
+    }
+  
+    fetchPacientes()
+  }, [])  
+  
   const handleSelectCita = (cita) => {
     setSelectedCita(cita)
     setShowDialog(true)
@@ -57,7 +96,7 @@ const CitasComponent = () => {
       cita: {
         medico_id: selectedCita.medico_id,
         especialidad_id: especialidadId,
-        paciente_id: pacienteId,
+        paciente_id: pacienteSeleccionado.id,
         estado: 1,
         dia: selectedCita.dia,
         hora: selectedCita.hora,
@@ -79,16 +118,34 @@ const CitasComponent = () => {
       return
     }
 
+    setErrorMsg('')
+    setInfoMsg('')
     if (response.ok) {
-      alert('Cita agendada exitosamente')
+      setInfoMsg('Cita agendada exitosamente')
+        setTimeout(() => {
+          setInfoMsg('')
+        }, 5000)
     } else {
       const err = await response.text()
-      alert(`Error al agendar cita: ${err}`)
-    }
+      setErrorMsg(`Error al agendar cita: ${err}`)
+      setTimeout(() => {
+        setErrorMsg('')
+      }, 5000)    }
   }
 
   return (
     <div className='p-6 bg-inf1 min-h-screen flex flex-col items-center overflow-auto h-screen'>
+      {infoMsg && (
+        <p className='text-green-700 bg-green-100 text-center mt-2 mb-4 px-4 py-2 rounded-lg font-semibold shadow'>
+          {infoMsg}
+        </p>
+      )}
+
+      {errorMsg && (
+        <p className='text-red-700 bg-red-100 text-center mt-2 mb-4 px-4 py-2 rounded-lg font-semibold shadow'>
+          {errorMsg}
+        </p>
+      )}
       <select
         className='w-full max-w-md p-3 border border-inf4 rounded-lg bg-white'
         value={especialidadId}
@@ -103,6 +160,21 @@ const CitasComponent = () => {
         {ESPECIALIDADES.map((esp) => (
           <option key={esp.id} value={esp.id}>
             {esp.nombre}
+          </option>
+        ))}
+      </select>
+      <select
+        className='w-full max-w-md p-3 border border-inf4 rounded-lg bg-white'
+        value={pacienteSeleccionado?.id || ''}
+        onChange={(e) => {
+          const id = parseInt(e.target.value)
+          const paciente = pacientes.find((p) => p.id === id)
+          setPacienteSeleccionado(paciente)
+        }}
+      >
+        {pacientes.map((p) => (
+          <option key={p.id} value={p.id}>
+            {p.nombre} {p.apellido_paterno}
           </option>
         ))}
       </select>
@@ -130,7 +202,8 @@ const CitasComponent = () => {
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
           <div className='bg-inf2 p-6 rounded-lg shadow-lg text-center m-6'>
             <p className='mb-4 text-lg font-semibold'>
-              ¿Agendar cita con {selectedCita.medico_nombre} el{' '}
+              ¿Agendar cita con {selectedCita.medico_nombre} para {' '}
+              {pacienteSeleccionado.nombre} {pacienteSeleccionado.apellido_paterno} el{' '}
               {selectedCita.dia} {selectedCita.fecha} a las {selectedCita.hora}?
             </p>
             <div className='flex justify-center gap-4'>
